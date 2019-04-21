180f9d95fe6978b34065f456cda438bc1bd02854
Peter Tempfli <tempflip@gmail.com>
2019-04-21 22:31:25 +0200

    w

--- tree.py
+++ tree.py
@@ -8,8 +8,11 @@ class ApexTopClass:
 	def __init__(self, content):
 		self.content = content
 		self.name = 'UNDEFINED'
+		self.inside_class_list = []
 
 		self.find_top_class_name()
+		self.get_all_inside_classes()
+		
 
 	def find_top_class_name(self):
 		pattern = re.compile('(public|global|private).+class ([a-zA-Z0-9_]+)', re.I)
@@ -20,15 +23,63 @@ class ApexTopClass:
 				self.name = m[2]
 				break 
 
+	def get_all_inside_classes(self):
+		pattern = re.compile('(public|global|private).+class ([a-zA-Z0-9_]+)', re.I)
+
+		for line in self.content:
+			m = pattern.match(line)
+			if m == None : continue
+			if m[2] == self.name : continue
+
+			self.inside_class_list.append(m[2])
+
+	def get_references_classes(self, class_list):
+
+		referenced_classes = []
+		for line in self.content:
+			pattern = re.compile('.*(' + '|'.join(class_list) + ')\.([a-zA-Z0-9_]+)\(.*\)', re.I)
+			m = pattern.match(line)
+			if m == None : continue
+			referenced_classes.append(m[1] + '.' + m[2])
+
+		return list(set(referenced_classes))
 
 	def __str__(self):
 		return ('Top level class: ' + self.name)
+
+class Codebase:
+	def __init__(self):
+		self.class_map = {}
+
+	def add_class(self, cls):
+		self.class_map[cls.name] = cls
+
+	def build_edges(self):
+		self.edges = {}
+		for classname, apexclass in self.class_map.items():
+			self.edges[classname] = []
+			for referenced_class in apexclass.get_references_classes(self.class_map.keys()):
+				self.edges[classname].append(referenced_class)
+
+	def print_edges(self):
+		for classname, edge_list in self.edges.items():
+			if not edge_list : continue
+
+			print ('### ', classname)
+
+			for edge in edge_list:
+				print ('\t\t' + edge)
+
+		print('--------------------------')
+
 
 def get_filelist(dir):
 
 	pattern = re.compile('.+cls$')
 
-	filelist = [dir + '/' + fn for fn in os.listdir(dir) if pattern.match(fn) != None]
+	exclude_pattern = re.compile('(.*test.*)|MetadataService', re.I)
+
+	filelist = [dir + '/' + fn for fn in os.listdir(dir) if (pattern.match(fn) != None and exclude_pattern.match(fn) == None)]
 	return filelist
 
 def read_file(fn):
@@ -41,13 +92,14 @@ def main():
 	code_dir = sys.argv[1]
 	filelist = get_filelist(code_dir)
 
-	class_list = []
+	cb = Codebase()
 
 	for fn in filelist:
 		apexClass = ApexTopClass(read_file(fn))
-		class_list.append(apexClass)
-	
-		print (apexClass)		
+		cb.add_class(apexClass)
+
+	cb.build_edges()
+	cb.print_edges()
 
 
 
